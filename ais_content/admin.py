71eb19206fdee85b694c853a1566f9d3d2bd6cdc
christian <chgad.games@gmail.com>
2021-01-23 18:21:37 +0100

    Implement Admin pages.

--- ais_content/admin.py
+++ ais_content/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Find, FormalAspect, Ware
+
+
+class FormalAspectInline(admin.StackedInline):
+    model = FormalAspect
+
+
+class WareInline(admin.StackedInline):
+    model = Ware
+
+
+class FindModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    inlines = [
+        FormalAspectInline,
+        WareInline
+    ]
+
+
+admin.site.register(FormalAspect, admin.ModelAdmin)
+admin.site.register(Ware, admin.ModelAdmin)
+admin.site.register(Find, FindModelAdmin)
