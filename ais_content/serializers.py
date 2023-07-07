d2d9ec104353ce2bd5ce2a3972ce0327a9d163c7
Christian Grossmüller <chgad.games@gmail.com>
2023-07-07 16:04:19 +0200

    Add GeneralInformationSerializer.

--- ais_content/serializers.py
+++ ais_content/serializers.py
@@ -2,7 +2,7 @@ from solid_backend.utils.serializers import SolidModelSerializer
 from solid_backend.media_object.serializers import MediaObjectSerializer
 from solid_backend.photograph.serializers import PhotographSerializer
 
-from .models import Find, FormalAspect, Ware
+from .models import Find, FormalAspect, Ware, GeneralInformation
 
 
 class FormalAspectSerializer(SolidModelSerializer):
@@ -17,7 +17,14 @@ class WareSerializer(SolidModelSerializer):
         exclude = ["find"]
 
 
+class GeneralInformationSerializer(SolidModelSerializer):
+    class Meta:
+        model = GeneralInformation
+        exclude = ["find"]
+
+
 class FindSerializer(SolidModelSerializer):
+    general_information = GeneralInformationSerializer()
     formal_aspect = FormalAspectSerializer()
     ware = WareSerializer(required=False)
     media_objects = MediaObjectSerializer(many=True)
